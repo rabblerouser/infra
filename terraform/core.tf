2503e8c0545472f55f505f33e6947f9f37766f9d
Cam Jackson <camjackson89@gmail.com>
2017-04-21 22:27:47 +1000

    Extract the kinesis publishing policy as its own thing

--- terraform/core.tf
+++ terraform/core.tf
@@ -22,22 +22,12 @@ resource "aws_iam_access_key" "core" {
   user = "${aws_iam_user.core.name}"
 }
 
-resource "aws_iam_user_policy" "put_record_to_rabblerouser_stream" {
-  name = "put_record_to_rabblerouser_stream"
+resource "aws_iam_user_policy_attachment" "core_put_to_stream" {
   user = "${aws_iam_user.core.name}"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [{
-      "Effect": "Allow",
-      "Action": "kinesis:PutRecord",
-      "Resource": "${aws_kinesis_stream.rabblerouser_stream.arn}"
-  }]
-}
-EOF
+  policy_arn = "${aws_iam_policy.put_to_stream.arn}"
 }
 
-resource "aws_iam_user_policy_attachment" "core_bucket_policy" {
+resource "aws_iam_user_policy_attachment" "core_read_bucket" {
   user = "${aws_iam_user.core.name}"
   policy_arn = "${aws_iam_policy.archive_bucket_readonly.arn}"
 }
