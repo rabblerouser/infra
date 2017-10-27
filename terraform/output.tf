f80ca032f9264550c15121f7a577fdffdeaf4570
Cam Jackson <camjackson89@gmail.com>
2017-10-27 20:58:30 +0800

    Hide RSA keys/certs from tf output

--- terraform/output.tf
+++ terraform/output.tf
@@ -13,3 +13,10 @@ output "docker_api_ca" {
 output "docker_api_cert" {
   value = "${module.base.docker_api_cert}"
 }
+
+###########################################################
+# NOTE! If you add any outputs here that are sensitive,   #
+# you must take care to make sure that they don't end     #
+# written to stdout during a `terraform apply`. You can   #
+# use `sed` for this - see the `tf` script for an example #
+###########################################################
