f241137064b20b5b8af03c40c0d7420c82e766cb
Cam Jackson <camjackson89@gmail.com>
2017-11-02 20:14:22 +0800

    Move some files around to better locations
    - Rename all output.tf to outputs.tf as per terraform standard convention
    - Move event-forwarder module inside docker-node-app module

--- terraform/apps/docker-node-app/event-forwarder.tf
+++ terraform/apps/docker-node-app/event-forwarder.tf
@@ -1,5 +1,5 @@
 module "app_event_forwarder" {
-  source = "../event-forwarder"
+  source = "./event-forwarder"
   name = "${var.name}"
   stream_arn = "${data.aws_kinesis_stream.stream.arn}"
   forward_to = "https://${aws_route53_record.app_domain.fqdn}/events"
